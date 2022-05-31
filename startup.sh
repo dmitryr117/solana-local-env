8e997e1ba836efc61f3466d6e619901e6a2b7274
dmitryr117 <colirs.developer@gmail.com>
2022-05-31 11:27:29 -0600

    Updated workspace

--- startup.sh
+++ startup.sh
@@ -3,9 +3,9 @@
 #useradd -m -u $HOST_UID -g $HOST_GID -o -s /bin/bash -s /root/.cargo/bin/cargo $HOST_USER
 
 # set npm registry to local Verdaccio
-npm set registry http://localhost:4873/
+# npm set registry http://localhost:4873/
 
 # original: https://registry.yarnpkg.com
-yarn config set npmRegistryServer http://localhost:4873/
+# yarn config set npmRegistryServer http://localhost:4873/
 
 tail -f /dev/null
